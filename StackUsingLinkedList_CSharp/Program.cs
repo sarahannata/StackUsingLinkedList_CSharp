ebcaaade41937dfc35b0b38bceaee6e0638b4d1d
sarahannata <sarahannata@gmail.com>
2022-11-30 16:02:11 +0700

    Menambahkan case 2

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -76,7 +76,14 @@ namespace StackUsingLinkedList_CSharp
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
-
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
         }
