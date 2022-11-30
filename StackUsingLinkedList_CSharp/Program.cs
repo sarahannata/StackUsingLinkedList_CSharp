0c7c746c68f8eefec07aafa2f6fc2035c0715f2d
sarahannata <sarahannata@gmail.com>
2022-11-30 16:00:48 +0700

    Menambahkan case 1

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -50,14 +50,35 @@ namespace StackUsingLinkedList_CSharp
                 //Traverse the list from beginning till end
                 for (tmp = top; tmp != null; tmp = tmp.next)
                 {
-                    Console.WriteLine(tmp.info)
+                    Console.WriteLine(tmp.info);
                 }
                 Console.WriteLine();
             }
         }
         static void Main(string[] args)
         {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop.");
+                Console.WriteLine("3. Display.");
+                Console.WriteLine("4. Exit.");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
 
+                }
+            }
         }
     }
 }
