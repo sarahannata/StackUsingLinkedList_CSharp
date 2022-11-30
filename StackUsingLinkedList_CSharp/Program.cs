b63c862b228d640fec6a58cc06c23ea64ef2a1fd
sarahannata <sarahannata@gmail.com>
2022-11-30 15:53:40 +0700

    Menambahkan public void display

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -40,5 +40,20 @@ namespace StackUsingLinkedList_CSharp
             Console.WriteLine("\nThe popped element is: " + top.info);
             top = top.next; //Make top point to the next node in sequence
         }
+        public void display()
+        {
+            Node tmp;
+            if (empty()) //If stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from beginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info)
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
